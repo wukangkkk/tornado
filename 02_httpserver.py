62aee6ab874e8d3c46d8ad402b326cdff9d1c331
xwp <xwp_fullstack@163.com>
2018-12-01 14:42:58 +0800

    改成多线程

--- 02_httpserver.py
+++ 02_httpserver.py
@@ -13,7 +13,11 @@ if __name__ == '__main__':
 	            (r"/",IndexHandler),                
 	                            ])
 	http_server=tornado.httpserver.HTTPServer(app)
-	http_server.listen(8000)
+	#修改成多进程
+	#http_server.listen(8000)
+	# http_server.start(num_processes=1)方法指定开启几个进程，参数num_processes默认值为1，即默认仅开启一个进程；如果num_processes为None或者<=0，则自动根据机器硬件的cpu核芯数创建同等数目的子进程；如果num_processes>0，则创建num_processes个子进程
+	http_server.bind(8000)
+	http_server.start(0)
 	tornado.ioloop.IOLoop.current().start()
 
 
